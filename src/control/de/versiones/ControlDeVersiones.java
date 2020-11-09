4ae6cdf4cf80b419632006bb8c859ab2394dbc9a
DAM129 <DAM129@IF03-17.iesmhp.local>
2020-11-09 16:36:35 +0100

    Control de versiones

---
+++ src/control/de/versiones/ControlDeVersiones.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control.de.versiones;
+
+/**
+ *
+ * @author DAM129
+ */
+public class ControlDeVersiones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("1. Además de Git, ¿que otros sistemas de control de versiones existen?");
+        System.out.println("2. En Git, ¿para qué sirve el comando git config?");
+        System.out.println("3. En Git, ¿para qué sirve el comando git init?");
+        System.out.println("4. En Git, ¿para qué sirve el comando git clone?");
+        System.out.println("5. En Git, ¿para qué sirve el comando git status?");
+        System.out.println("6. En Git, ¿para qué sirve el comando git add?");
+        
+        // DAM129 Vladyslav Golovatyi Tsymbal
+        
+    }
+    
+}
